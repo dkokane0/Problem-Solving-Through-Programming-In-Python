97e5a0863fb56d4d306db73623992e9fd23858d5
Deepak Kokane <68640332+dkokane0@users.noreply.github.com>
2021-12-09 11:36:57 +0530

    Update fibo_series.py

--- fibo_series.py
+++ fibo_series.py
@@ -1,1 +1,22 @@
-print("h")
+# Program to display the Fibonacci sequence up to n-th term
+n_terms = int(input("How many terms? "))
+# first two terms
+n1, n2 = 0, 1
+count = 0
+# check if the number of terms is valid
+if n_terms <= 0:
+   print("Please enter a positive integer")
+# if there is only one term, return n1
+elif n_terms == 1:
+   print("Fibonacci sequence upto",n_terms,":")
+   print(n1)
+# generate fibonacci sequence
+else:
+   print("Fibonacci sequence:")
+   while count < n_terms:
+       print(n1)
+       nth = n1 + n2
+       # update values
+       n1 = n2
+       n2 = nth
+       count += 1
